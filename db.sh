da6d349e6d319bda89efb16cd7354c0a0786d267
Haf <haf@protonmail.ch>
2022-12-20 11:33:55 +0100

    using dollar database

--- db.sh
+++ db.sh
@@ -45,9 +45,8 @@ db_resource="${1?Must provide resource}"
 
 # XXX: This is for Zalando's PostgreSQL operator, but might need other paths for something else
 json_db_to_user="$(kubectl ${namespace:+-n $namespace} get pg "$db_resource" -o json | jq '.spec.databases | to_entries[]' )"
-# TODO: Why am I not using $database? How is it that I don't need it?
 database="$(jq -r .key <<<"$json_db_to_user")"
 user="$(jq -r .value <<<"$json_db_to_user")"
 password="$(kubectl ${namespace:+-n $namespace} get secret "$user.$db_resource.credentials.postgresql.acid.zalan.do" -o 'jsonpath={.data.password}' | base64 -d)"
 
-kubectl ${namespace:+-n $namespace} run "$db_resource-cli" --rm -i --image governmentpaas/psql -- psql "postgresql://$user:$password@$db_resource.${namespace:+$namespace.}svc:5432"
+kubectl ${namespace:+-n $namespace} run "$db_resource-cli" --rm -i --image governmentpaas/psql -- psql "postgresql://$user:$password@$db_resource.${namespace:+$namespace.}svc:5432/$database"
